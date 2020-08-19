0140afd0f548eda7398d5871f9c3d580435dfbc9
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-08-20 00:24:38 +0200

    feat(init): initial setup for future integrations

---
+++ db_setup/init-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER emeis WITH PASSWORD '$EMEIS_DB_PASSWORD';
+    CREATE DATABASE emeis OWNER emeis;
+    ALTER USER emeis WITH SUPERUSER;
+    CREATE USER alexandria WITH PASSWORD '$ALEXANDRIA_DB_PASSWORD';
+    CREATE DATABASE alexandria OWNER alexandria;
+    ALTER USER alexandria WITH SUPERUSER;
+EOSQL
---
+++ ember/app/routes/application.js
@@ -0,0 +1,11 @@
+import { inject as service } from '@ember/service';
+import Route from "@ember/routing/route";
+
+export default class ApplicationRoute extends Route {
+  @service intl;
+
+  beforeModel(...args) {
+    super.beforeModel(...args);
+    this.intl.setLocale(["en"]);
+  }
+}
