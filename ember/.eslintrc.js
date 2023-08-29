494d9e19bb6cf1ed44ac6fa05bf49d0e06660fdb
Mitan Omar <mitan@gmx.ch>
2023-08-29 09:22:17 +0200

    chore: upgrade ember to 4.12

--- ember/.eslintrc.js
+++ ember/.eslintrc.js
@@ -1,5 +1,56 @@
 "use strict";
 
 module.exports = {
-  extends: ["@adfinis/eslint-config/ember-app"],
+  root: true,
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: true }],
+      ],
+    },
+  },
+  plugins: ["ember"],
+  extends: [
+    "eslint:recommended",
+    "plugin:ember/recommended",
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {},
+  overrides: [
+    // node files
+    {
+      files: [
+        "./.prettierrc.js",
+        "./.eslintrc.js",
+        "./.stylelintrc.js",
+        "./.template-lintrc.js",
+        "./ember-cli-build.js",
+        "./testem.js",
+        "./blueprints/*/index.js",
+        "./config/**/*.js",
+        "./lib/*/index.js",
+        "./server/**/*.js",
+      ],
+      parserOptions: {
+        sourceType: "script",
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      extends: ["plugin:n/recommended"],
+    },
+    {
+      // test files
+      files: ["tests/**/*-test.{js,ts}"],
+      extends: ["plugin:qunit/recommended"],
+    },
+  ],
 };
--- ember/ember-cli-build.js
+++ ember/ember-cli-build.js
@@ -5,43 +5,10 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    sourcemaps: { enabled: true },
-    "ember-cli-babel": {
-      includePolyfill: true,
-    },
-
-    sassOptions: {
-      includePaths: [
-        // For some reason sass cant find these on its own :/
-        "node_modules/ember-power-select/app/styles/",
-        "node_modules/ember-basic-dropdown/app/styles/",
-      ],
-    },
     emberApolloClient: {
-      keepGraphqlFileExtension: false,
-    },
-    "ember-fetch": {
-      preferNative: true,
-    },
-    fingerprint: {
-      // disable fingerprinting for images to make sure logo
-      // config path doesn't break in production
-      extensions: ["js", "css", "map"],
-    },
+      keepGraphqlFileExtension: false
+    }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
