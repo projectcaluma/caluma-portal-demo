008a443924264f618499304cd5bee487fad961e2
Mitan Omar <mitan@gmx.ch>
2023-09-04 22:08:57 +0200

    fix: scss linter error

--- ember/.stylelintrc.js
+++ ember/.stylelintrc.js
@@ -2,4 +2,8 @@
 
 module.exports = {
   extends: ["stylelint-config-standard", "stylelint-prettier/recommended"],
+  rules: {
+    "import-notation": null,
+    "at-rule-no-unknown": null,
+  },
 };
