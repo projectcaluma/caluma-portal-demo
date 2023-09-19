34b64edc0fced7e26d05126fafab89ecaed330e6
Mitan Omar <mitan@gmx.ch>
2023-09-19 11:27:17 +0200

    fix: disable fingerprinting the assets to fix caluma logo in production

--- ember/ember-cli-build.js
+++ ember/ember-cli-build.js
@@ -3,6 +3,9 @@
 const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function (defaults) {
+  defaults.fingerprint = {
+    enabled: false,
+  };
   const app = new EmberApp(defaults, {
     emberApolloClient: {
       keepGraphqlFileExtension: false,
